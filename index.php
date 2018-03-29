0058a0f9199374a446ddb9510798cf19be0d67a1
pasak <pasaksoftware@gmail.com>
2018-03-29 13:58:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include ('line-bot-api/php/line-bot.php');
+$channelSecret = 'ecac69de83a8b91495dd961c1b49f4a5';
+$access_token  = '0zhyZeKzFbPrHc3wsukcNfHJngX61gJnyJBjCAMdiZGlro3eJFu3s4eP1FM3t9psiiHlnZYG2FgRgmgIOFMK0HiPcFTxXshD9eN3Ir+rNe1Cci10aV5Y1pDJPvBvPHoNKXDDxcQT9VXotv9vcpzoDgdB04t89/1O/w1cDnyilFU=';
+$bot = new BOT_API($channelSecret, $access_token);
+	
+if (!empty($bot->isEvents)) {
+		
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
+        exit();
+    }
+    // Failed
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+    exit();
+}
+?>
